28cf2bcd313e71cb9f770d825a07b8a9785bccab
Abdelmonm Alsnajleh <aalsnajleh@gmail.com>
2025-11-20 02:00:39 +0300

    Added redirection

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -9,47 +9,39 @@ use Illuminate\Support\Facades\Hash;
 class AuthController extends Controller
 {
    public function login(Request $request)
-{
-    $request->validate([
-        'email' => 'required|email',
-        'password' => 'required'
-    ]);
-
-    $credentials = $request->only('email', 'password');
-
-    if (auth()->attempt($credentials)) {
-        $request->session()->regenerate();
-
-    return redirect('/dashboard');
-
+   {
+        if (auth()->check()) {
+            return redirect('/dashboard');
+        }
+        $request->validate([
+            'email' => 'required|email',
+            'password' => 'required'
+        ]);
+        $credentials = $request->only('email', 'password');
+        if (auth()->attempt($credentials)) {
+            $request->session()->regenerate();
+            return redirect('/dashboard');
+        }
+        return back()->withErrors([
+            'email' => 'Invalid login credentials.',
+        ]);
     }
-
-    return back()->withErrors([
-        'email' => 'Invalid login credentials.',
-    ]);
-}
-
     
-public function register(Request $request)
-{
-    $request->validate([
-        'name' => 'required',
-        'email' => 'required|email|unique:users,email',
-        'password' => 'required|min:6|confirmed'
-    ]);
-
-    $user = User::create([
-        'name' => $request->name,
-        'email' => $request->email,
-        'password' => Hash::make($request->password)
-    ]);
-
-    auth()->login($user);
-
-    return redirect()
-        ->to('/welcome')   // الصفحة اللي بعد التسجيل
-        ->with('success', 'Account created successfully!');
+    public function register(Request $request)
+    {
+        $request->validate([
+            'name' => 'required',
+            'email' => 'required|email|unique:users,email',
+            'password' => 'required|min:6|confirmed'
+        ]);
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password)
+        ]);
+        auth()->login($user);
+        return redirect()
+            ->to('/welcome')   // الصفحة اللي بعد التسجيل
+            ->with('success', 'Account created successfully!');
+    }
 }
-
-}
-
